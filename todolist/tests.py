6f9417ddc6bc26749b0a667a92d712c6acc568e6
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2024-05-26 19:59:20 +0200

    test

--- todolist/tests.py
+++ todolist/tests.py
@@ -1,3 +1,13 @@
+from django.test import Client
 from django.test import TestCase
+import unittest
+from django.contrib.auth.models import User
+from django.test import Client
 
-# Create your tests here.
+class URLTests(TestCase):    
+    def test_testhomepage(self):
+        self.client = Client()
+        self.user = User.objects.create_user('Sergej', password='Yaq12wsx!')
+        self.client.login(username='Sergej', password='Yaq12wsx!')
+        response = self.client.get('/todos/')
+        self.assertEqual(response.status_code, 200)
